7949d2509665bf31501506fac8ff4e8760e426ec
melindagallo <melinda.gallo@4d.com>
2014-01-24 17:58:01 +0100

    Update designer.js
    Updated for latest version of Wakanda MAIN build 152974

--- designer.js
+++ designer.js
@@ -1,52 +1,45 @@
-(function() {
-    var Hyperlink = Widget.Hyperlink.inherit(WAF.require('waf-behavior/studio'));
+(function(Hyperlink) {
 
-    /*Widget's display name*/
-    Hyperlink.setDescription('Hyperlink');
-
-    /*Properties*/
-    Hyperlink.addAttributes([{
-        name: 'data-binding',
-        description: 'Hyperlink Source'
-    },{
-        name: 'data-binding-label',
-        description: 'Hyperlink Text',
-        typeValue: 'datasource'
-    },{
-        name: 'data-target',
-        description: 'Target',
-        defaultValue: '_blank',
-        type: 'dropdown',
-        options: ['_blank', '_self']
-    },{
-        name: 'data-text',
-        description: 'Default Text',
-        type: 'textarea'
-    },{
-        name: 'data-label',
-        description: 'Label',
-        type: 'string'
-    }]);
-
-    /*Default label position*/
-	Hyperlink.setLabelPosition("left");
-
-    /*Default size*/
+    /*Default width and height of your widget when added to the Page*/
     Hyperlink.setWidth('200');
     Hyperlink.setHeight('20');
 
-    /*Events*/
-    Hyperlink.addEvents([{
-        'name': 'click',
-        'description': 'On Click',
-        'category': 'Mouse Events'
-    },{
-        'name': 'mouseover',
-        'description': 'On Mouse Over',
-        'category': 'Mouse Events'
-    }]);
+    /*Customize existing properties*/
+    Hyperlink.customizeProperty('url', {
+        sourceTitle: 'URL Source',
+        description: 'Bind a datasource containing a URL or email.',
+        display: false,
+        sourceDisplay: true
+    });
+    Hyperlink.customizeProperty('urlText', {
+        sourceTitle: 'URL text Source',
+        description: 'Bind a datasource containing the text to display for the hyperlink.',
+        display: false,
+        sourceDisplay: true
+    });
+    Hyperlink.customizeProperty('defaultText', {
+        title: 'Default text',
+        type: 'textarea',
+        description: 'Default text to display if the URL Text Source is blank.',
+        display: true,
+        sourceDisplay: false
+    });
+    Hyperlink.customizeProperty('target', {
+        title: 'Target',
+        description: 'Default text to display if the URL Text Source is blank.',
+        type: 'dropdown',
+        options: ['_blank', '_self'],
+        display: true,
+        sourceDisplay: false
+    });
 
-    /*Styles*/
+    /*Create label property*/
+    Hyperlink.addLabel({
+        defaultValue: '',
+        position: 'left'
+    });
+
+    /*Set the Design and Styles panels*/
     Hyperlink.setPanelStyle({
         'fClass': true,
         'text': true,
@@ -56,11 +49,12 @@
         'label': true,
         'disabled': ['border-radius']
     });
-        
-    Hyperlink.on('Display', function(event) {
-    	if(event['data-binding']) { 
-     	   $('#' + event['id']).html('['+event['data-binding']+']');
-    	}
-    });
 
-})();
+    //display the datasource selected inside the widget
+    Hyperlink.on('Display', function(event) {
+        if (event['data-binding-url']) {
+            $('#' + event['id']).html('[' + event['data-binding-url'] + ']');
+        }
+    });
+    
+});
